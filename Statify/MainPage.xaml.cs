59a3138d6fc96bffce35e97da61b8358f1d39622
David Ortinau <david.ortinau@microsoft.com>
2019-01-20 13:38:21 -0600

    VisualStateManager example. Tap to change states

---
+++ Statify/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Statify
+{
+    public partial class MainPage : ContentPage
+    {
+        string curState = "Red";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SetState(curState);
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            curState = (curState == "Blue") ? "Red" : "Blue";
+            SetState(curState);
+        }
+
+        private void SetState(string visualState)
+        {
+            VisualStateManager.GoToState(this, visualState);
+            VisualStateManager.GoToState(MyLabel, visualState);
+        }
+    }
+}
